3265a427c3f5cfe21430ffd58c9545dad3737d91
satoumiu <Miustys11@gmail.com>
2020-05-06 09:29:03 +0000

    PHP/laravel 11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,6 +8,22 @@
         <title>MyProfile</title>
     </head>
     <body>
-        <h1>Myプロフィール作成画面</h1>
+        {{-- layouts/profile.blade.phpを読み込む --}}
+　　　　@extends('layouts.profile')
+
+
+　　　　{{-- admin.blade.phpの@yield('title')に'My プロフィール'を埋め込む --}}
+　　　　@section('title', 'My プロフィール')
+
+　　　　{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+　　　　@section('content')
+   　　　     <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>My プロフィール</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection
     </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,20 @@
-let mix = require('laravel-mix');
+// let mix = require('laravel-mix');
+
+// /*
+//  |--------------------------------------------------------------------------
+//  | Mix Asset Management
+//  |--------------------------------------------------------------------------
+//  |
+//  | Mix provides a clean, fluent API for defining some Webpack build steps
+//  | for your Laravel application. By default, we are compiling the Sass
+//  | file for the application as well as bundling up all the JS files.
+//  |
+//  */
+
+// mix.js('resources/assets/js/app.js', 'public/js')
+//   .sass('resources/assets/sass/app.scss', 'public/css');
+   
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +27,6 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css')
+   .sass('resources/sass/profile.scss', 'public/css');
