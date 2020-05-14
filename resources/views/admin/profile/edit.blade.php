a1dbca3c9aa544db032ce1dc4e88bcce95632183
satoumiu <Miustys11@gmail.com>
2020-05-14 12:00:03 +0000

    PHP/laravel14

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -16,7 +16,7 @@
                 <div class="row">
                     <div class="col-md-8 mx-auto">
                         <h2>Myプロフィール編集画面</h2>
-                        <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
+                        <form action="{{ action('Admin\ProfileController@edit') }}" method="post" enctype="multipart/form-data">
         
                             @if (count($errors) > 0)
                                 <ul>
